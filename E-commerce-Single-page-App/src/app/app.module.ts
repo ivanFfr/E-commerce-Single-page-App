fb3a1657f29f29434b77a8f01dca51358c9424d4
ivanFfr <ivanfilipov45@gmail.com>
2017-06-24 16:52:47 +0300

    added router

--- E-commerce-Single-page-App/src/app/app.module.ts
+++ E-commerce-Single-page-App/src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './views/semantic-elements/header/header.component'
+import { NavigationComponent } from './views/semantic-elements/navigation/navigation.component'
+import { FooterComponent } from './views/semantic-elements/footer/footer.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './views/semantic-elements/main/main.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [
@@ -12,8 +29,17 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase, 'E-commerce-Single-page-App'),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomeComponent,
+    MainComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
